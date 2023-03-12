a460666af1d7966b11c140d967d28021c07b5435
SushiNspice <113986306+sue-lim@users.noreply.github.com>
2023-03-12 15:58:16 +0800

    Footer Updated

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 // Styles
 import "./Nav.css";
 
-
 function Nav(props) {
 const { loggedIn, setLoggedIn } = props;
 
@@ -32,12 +31,15 @@ const handleClick = () => {
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
                 </a>
 
-                <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-                <span className="sr-only">Open main menu </span>
+                <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false"aria-haspopup="true">
+                {/* <span className="sr-only">Open main menu </span> */}
             
                 
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" ></path></svg>
+                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" >
+                    </path>
+                    </svg>
                 </button>
+                
 
                 <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
 
@@ -48,23 +50,15 @@ const handleClick = () => {
 
                     <ul className="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
-                            <Link className="nav-bar-link-horizontal" to="/">
-                            Home
+                            <Link className="nav-bar-link-horizontal" to="/"> Home
                             </Link>
                         </li>
                         <li>
-                            <Link className="nav-bar-link-horizontal" to="/all-projects">
-                            Projects
+                            <Link className="nav-bar-link-horizontal" to="/all-projects"> Projects
                             </Link>
                         </li>
                         <li>
-                            <Link className="nav-bar-link-horizontal" to="/about-us">
-                            About 
-                            </Link>
-                            </li>
-                        <li>
-                            <Link className="nav-bar-link-horizontal" to="/contact">
-                            Contact
+                            <Link className="nav-bar-link-horizontal" to="/about-us"> About 
                             </Link>
                         </li>
                         <li>
